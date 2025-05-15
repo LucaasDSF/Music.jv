7a001e030250a9d03443d07cdcc403bc313b7e54
Lucas S. França <164523347+LucaasDSF@users.noreply.github.com>
2025-05-15 12:25:25 -0300

    Update Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,21 @@
-public class Main {
+public class Principal {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Musica minhaMusica= new Musica();
+
+        minhaMusica.titulo = " OH SE FENDESSE ";
+        minhaMusica.artista = " MORADA ";
+        minhaMusica.anoLancamento =  2024;
+
+        minhaMusica.exibeFichaTecnica();
+
+        minhaMusica.avalia(4.8);
+        minhaMusica.avalia(5.0);
+        minhaMusica.avalia(4.9);
+
+        double mediaAvaliacoes = minhaMusica.pegaMedia();
+        System.out.println(" Média das avaliações" + mediaAvaliacoes);
+
+
     }
 }
